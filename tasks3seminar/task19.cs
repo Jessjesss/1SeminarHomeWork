4910ed869d5e2e1a0aa95075986998e7db953abf
EvgeniaKozlova <photofridzon@gmail.com>
2022-09-27 11:49:22 +0700

    3 seminar

---
+++ tasks3seminar/task19.cs
@@ -0,0 +1,22 @@
+﻿/* 19. Напишите программу, 
+которая принимает на вход 
+пятизначное число и проверяет, 
+является ли оно палиндромом. */
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+string numInd = Convert.ToString(number);
+
+if (number < 10000 | number > 100000)
+{
+  Console.WriteLine("Некорректное число!");
+}
+else if (numInd[0] == numInd[4] && numInd[1] == numInd[3])
+  {
+  Console.WriteLine("Это палиндром!");
+  }
+else
+  {
+  Console.WriteLine("Это не палиндром!");
+  }
+
---
+++ tasks3seminar/task21.cs
@@ -0,0 +1,27 @@
+﻿/* 21. Напишите программу, которая принимает
+ на вход координаты двух точек и находит
+  расстояние между ними в 3D пространстве. */
+
+/* A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+
+int ReadLine(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = ReadLine("Введите координату X первой точки: ");
+int y1 = ReadLine("Введите координату Y первой точки: ");
+int z1 = ReadLine("Введите координату Z первой точки: ");
+int x2 = ReadLine("Введите координату X второй точки: ");
+int y2 = ReadLine("Введите координату Y второй точки: ");
+int z2 = ReadLine("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double s = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Расстояние между точками: {s:f2}");
