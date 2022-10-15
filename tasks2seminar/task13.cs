579df42ba8bc1ead68820a2838cbe9da873ff6fd
Jessjesss <113085452+Jessjesss@users.noreply.github.com>
2022-10-15 13:44:28 +0700

    Update task13.cs

--- tasks2seminar/task13.cs
+++ tasks2seminar/task13.cs
@@ -8,20 +8,16 @@
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
-if (number > 99 && number < 999)
-{
+if (number > 99)
+    {
+while(number > 999)
+    {
+    number = number / 10;
+    }
     int result = number % 10;
     Console.Write($"Третьим числом является: {result}");
-}
-else if (number > 999 && number < 9999)
-{
-    int result = number % 100 / 10;
-    Console.Write($"Третьим числом является: {result}");
-}
-else if (number > 9999 && number < 99999)
-{
-    int result = (number / 10) % 100 / 10;
-    Console.Write($"Третьим числом является: {result}");
-}
+    }
 else
+    {
     Console.Write("Третьей цифры нет!");
+    }
